41b9cec51d72b5945ceb325b2f3ddf33312ba770
M Aleem <162026993+Aleem12142@users.noreply.github.com>
2024-03-29 22:34:05 -0700

    added removed index command

--- index.js
+++ index.js
@@ -2,21 +2,40 @@
 import inquirer from "inquirer";
 let todos = [];
 let condition = true;
-while (condition) {
-    let addTask = await inquirer.prompt([
-        {
-            name: "todo",
-            type: "input",
-            message: "What you want add in your Todos?"
-        },
-        {
-            name: "addMore",
-            type: "confirm",
-            message: "Do you want to add more?",
-            default: "false"
-        }
-    ]);
-    todos.push(addTask.todo);
-    condition = addTask.addMore;
-    console.log(todos);
+async function main() {
+    while (condition) {
+        let addTask = await inquirer.prompt([
+            {
+                name: "todo",
+                type: "input",
+                message: "What do you want to add to your Todos?"
+            },
+            {
+                name: "addMore",
+                type: "confirm",
+                message: "Do you want to add more?",
+                default: false
+            }
+        ]);
+        todos.push(addTask.todo);
+        condition = addTask.addMore;
+        console.log(todos);
+    }
+    // After adding todos, ask if the user wants to remove any item
+    const removeItem = await inquirer.prompt({
+        name: "removeIndex",
+        type: "number",
+        message: "Enter the index of the item you want to remove from the Todos list (0 to cancel):",
+        validate: (input) => input >= 0 && input < todos.length + 1
+    });
+    // If the user entered a valid index (not 0), remove the item
+    if (removeItem.removeIndex !== 0) {
+        const removedItem = todos.splice(removeItem.removeIndex - 1, 1);
+        console.log(`Item '${removedItem[0]}' removed from the Todos list.`);
+        console.log(todos);
+    }
+    else {
+        console.log("No item was removed.");
+    }
 }
+main();
--- index.ts
+++ index.ts
@@ -2,27 +2,45 @@
 
 import inquirer from "inquirer";
 
-let todos = [];
-let condition = true;
+let todos: string[] = [];
+let condition: boolean = true;
 
-while(condition){
-    let addTask =await inquirer.prompt(
-        [
+async function main() {
+    while (condition) {
+        let addTask = await inquirer.prompt([
             {
                 name: "todo",
                 type: "input",
-                message: "What you want add in your Todos?"
+                message: "What do you want to add to your Todos?"
             },
             {
                 name: "addMore",
                 type: "confirm",
                 message: "Do you want to add more?",
-                default: "false"
+                default: false
             }
-        ]
-    );
-    todos.push(addTask.todo);
-    condition = addTask.addMore;
-    console.log(todos);
-    
+        ]) as { todo: string, addMore: boolean };
+        todos.push(addTask.todo);
+        condition = addTask.addMore;
+        console.log(todos);
+    }
+
+    // After adding todos, ask if the user wants to remove any item
+    const removeItem = await inquirer.prompt({
+        name: "removeIndex",
+        type: "number",
+        message: "Enter the index of the item you want to remove from the Todos list (0 to cancel):",
+        validate: (input: number) => input >= 0 && input < todos.length + 1
+    });
+
+    // If the user entered a valid index (not 0), remove the item
+    if (removeItem.removeIndex !== 0) {
+        const removedItem = todos.splice(removeItem.removeIndex - 1, 1);
+        console.log(`Item '${removedItem[0]}' removed from the Todos list.`);
+        console.log(todos);
+    } else {
+        console.log("No item was removed.");
+    }
 }
+
+main();
